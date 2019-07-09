ae61c68210782259db5ef7ac83cf16b0325f1a62
maishamostofa <51942065+maishamostofa@users.noreply.github.com>
2019-07-09 11:15:52 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,48 +1,35 @@
-#include<iostream>
+#include <iostream>
 #include<fstream>
-#include<cstdlib>
-#include<ctime>
 using namespace std;
-void insertion_sort(int arr[],int n)
-{
-    int i,key,index;
-    for(i=1;i<n;i++)
-    {
-      key=arr[i];
-      index=i-1;
-      while((index>=0) &&  (arr[index]>key))
-      {
-          arr[index+1]=arr[index];
-          index=index-1;
-      }
-      arr[index+1]=key;
-    }
-}
+
 int main()
 {
-    int n=10000;
-    ofstream fileInput;
-    srand(89345);
-    fileInput.open("Input(10000).txt");
-    int*Array=new int[n];
-    for(int i=0;i<n;i++)
+    int row, column;
+    ifstream fileInput;
+    fileInput.open("Input.txt");
+    fileInput>>row>>column;
+    int **Array;
+    Array=new int*[row];
+    for(int i=0; i<row; i++)
     {
-        Array[i]=rand();
-        fileInput<<i+1<<") "<<Array[i]<<endl;
+        Array[i]=new int[column];
     }
-    fileInput.close();
-    clock_t startTime=clock();
-    insertion_sort(Array,n);
-    clock_t endTime=clock();
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<column; j++)
+        {
+            fileInput>>Array[i][j];
+        }
+    }
     ofstream fileOutput;
-    fileOutput.open("Output2(10000).txt");
-    for(int i=0;i<n;i++)
+    fileOutput.open("Output.txt");
+    for(int i=0; i<column; i++)
     {
-        fileOutput<<i+1<<") "<<Array[i]<<endl;
+        for(int j=0; j<row; j++)
+        {
+            fileOutput<<Array[j][i]<<" ";
+        }
+        fileOutput<<endl;
     }
-    double timelapse=double (endTime-startTime)/CLOCKS_PER_SEC;
-    cout<<"timelapse: "<<timelapse<<endl;
-    fileOutput<<endl<<"timelapse: "<<timelapse;
-    fileOutput.close();
-    delete[] Array;
+    return 0;
 }
